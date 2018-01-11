30d066f70fd81feb9490878de1d593fe2bbcc9a7
aidanr002 <arya6609@SHARED.SYDNEY.EDU.AU>
2018-01-11 17:39:41 +1100

    Removed un-needed recommendations and added user ID not found

--- server.py
+++ server.py
@@ -2,17 +2,16 @@
 
 from tornado.ncss import Server, ncssbook_log
 
-def get_recommendations(user_id):
-    recommendations = {
-1: ["Google", "Wisetech", "Freelancer"], 2: ["Macquarie", "Commonwealth Bank", "Optiver"]}
-
 def index_handler (request):
     request.write("Placeholder")
 
 def profile_handler (request, user_id):
-    with open('profile.html') as p:
-        profile_html = p.read()
-        request.write(profile_html)
+    if user_id == '1':
+        with open('profile.html') as p:
+            profile_html = p.read()
+            request.write(profile_html)
+    else:
+        request.write("User Not found")
 
 server = Server()
 server.register(r'/', index_handler)
