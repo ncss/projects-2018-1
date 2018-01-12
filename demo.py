218453b3b31cc155f849741bf78b3fea3190f130
thardy2001 <thar6624@SHARED.SYDNEY.EDU.AU>
2018-01-12 16:40:04 +1100

    Created temp create user page and edited server

--- demo.py
+++ demo.py
@@ -1,3 +1,11 @@
 from templating import render
 
-print(render("{{hello}}", {"hello": "WOw!"}))
+print(render("<h1>{{hello}}</h1>", {"hello": "WOw!"}))
+
+
+'''
+    First Parameter is The HTML file that we need to render.
+    E.g: profile_html = render(profile_html, {"test": "Hello, World!"})
+
+    Second Parameter must be a dictionary, the key is a variable name and the value is whath the variable is initialised to .
+'''
--- server.py
+++ server.py
@@ -3,6 +3,10 @@ from templating import render
 from tornado.ncss import Server, ncssbook_log # ncssbook_log --> Optional | The logs will be more legible and easyer to follow / understand
 from database.seeker import Seeker
 
+
+user = Seeker(1,"James","Curran", "1/1/2012", "000", "james@ncss.com", "Sydney", ["Univeristy of Sydney - Bachelor of Science", "PhD in Computing Linguistics @ Sydeny Univeristy"], ["Coding","Running buisinesses","Reading storiess", "spelling"], ["Python", "everythgin"], ["NCSS"])
+
+
 def index_handler (request):
     with open('index.html') as i:
         index_html = i.read()
@@ -10,13 +14,14 @@ def index_handler (request):
 
 def profile_handler (request, user_id):
     if user_id == '1':
-        test_user = Seeker(1,"James","Curran", "1/1/2012", "000", "james@ncss.com", "Sydney", "Univeristy of Sydney - Bachelor of Science,PhD in Computing Linguistics @ Sydeny Univeristy", "Coding, Running buisinesses, Reading stories, spelling", "Python, everything","NCSS")
         with open('profile.html') as p:
             profile_html = p.read()
-            profile_html = render(profile_html, {"user": test_user,})
+            profile_html = render(profile_html, {"test": user})
             request.write(profile_html)
     else:
-        request.write("User Not found")
+        with open("usernotfound.html") as u:
+            usernotfound_html = u.read()
+            request.write(usernotfound_html)
 
 def about_handler(request):
     request.write("Page Under Construction")
@@ -30,6 +35,17 @@ def position_handler(request, page_id):
 def map_handler(request):
     request.write("Page Under Construction")
 
+# Handler to display the form
+def ESP_handler(request):
+    with open('ExampleSeekerProfile.html') as i:
+        ESP_html = i.read()
+        request.write(ESP_html) # TEMPORARY STAND IN HTML AND CSS FILE
+
+# Handler for creating a new profile (for submiting the form, handle the returned post)
+def new_esp_handler(request):
+    variable = request.get_field("username")
+    print(variable)
+
 server = Server() # Create a server object
 server.register(r'/', index_handler)
 server.register(r'/about/', about_handler)
@@ -37,4 +53,5 @@ server.register(r'/searchresult/', searchresult_handler)
 server.register(r'/positioninformation/(\d+)', position_handler) # Dynamic page | takes in a user id which is used
 server.register(r'/profile/(\d+)/', profile_handler) # Dynamic page | takes in a user id which is used
 server.register(r'/map/', map_handler)
+server.register(r'/ExampleSeekerProfile/', ESP_handler, post = new_esp_handler)
 server.run() # Runs Server
