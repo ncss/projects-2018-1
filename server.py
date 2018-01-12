fc0b7f1c52a3696410fba206356ca07623cfe8d3
thardy2001 <thar6624@SHARED.SYDNEY.EDU.AU>
2018-01-12 12:46:29 +1100

    Implemented Demo Templating Engine

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 # The Server
-
-from tornado.ncss import Server, ncssbook_log
+from templating import render
+from tornado.ncss import Server, ncssbook_log # ncssbook_log --> Optional | The logs will be more legible and easyer to follow / understand
 
 
 def index_handler (request):
@@ -12,6 +12,7 @@ def profile_handler (request, user_id):
     if user_id == '1':
         with open('profile.html') as p:
             profile_html = p.read()
+            profile_html = render(profile_html, {"test": "Hello, World!"})
             request.write(profile_html)
     else:
         request.write("User Not found")
@@ -28,11 +29,11 @@ def position_handler(request, page_id):
 def map_handler(request):
     request.write("Page Under Construction")
 
-server = Server()
+server = Server() # Create a server object
 server.register(r'/', index_handler)
 server.register(r'/about/', about_handler)
 server.register(r'/searchresult/', searchresult_handler)
-server.register(r'/positioninformation/(\d+)', position_handler)
-server.register(r'/profile/(\d+)/', profile_handler)
+server.register(r'/positioninformation/(\d+)', position_handler) # Dynamic page | takes in a user id which is used
+server.register(r'/profile/(\d+)/', profile_handler) # Dynamic page | takes in a user id which is used
 server.register(r'/map/', map_handler)
-server.run()
+server.run() # Runs Server
