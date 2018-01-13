377071cd18fdb8aedbcaaf69cb5b5c200740b59e
Alexander Edwards <aedw4976@shared.sydney.edu.au>
2018-01-13 20:45:20 +1100

    Added companies to setup.py - populated from JSON file.

--- database/company.sql
+++ database/company.sql
@@ -2,9 +2,8 @@ CREATE TABLE company (
     id INTEGER PRIMARY KEY NOT NULL,
     name TEXT NOT NULL,
     url TEXT NOT NULL,
-
     formality INTEGER NOT NULL,
     finance INTEGER NOT NULL,
     difficulty INTEGER NOT NULL,
-    size INTEGER NOT NULL,
+    size INTEGER NOT NULL
 );
