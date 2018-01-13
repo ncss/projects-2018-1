9d0089a7947faf82e83a5204d1a897afb4e4735a
Bella2357 <brontelee@rocketmail.com>
2018-01-14 05:32:17 +1100

    Positions set up is now working

--- database/setup.py
+++ database/setup.py
@@ -4,7 +4,7 @@ import os
 
 f = os.path.dirname(os.path.abspath(__file__))
 print(f)
-conn = sqlite3.connect(os.path.join(f, "../seekers_personal.db"))
+conn = sqlite3.connect(os.path.join(f, "./seekers_personal.db"))
 cur = conn.cursor()
 
 cur.executescript(open(os.path.join(f, "seekers_personal.sql"), 'rU').read())
@@ -17,6 +17,12 @@ for company in json.loads(open(os.path.join(f, 'companies.json')).read()):
                    VALUES ('{}','{}','{}','{}','{}','{}')""".format(
                    company['name'], company['url'], company['formality'],
                    ','.join(company['languages']), ','.join(company['locations']), company['size']))
+
+for position in json.loads(open(os.path.join(f, 'positions.json')).read()):
+    cur.execute("""INSERT INTO position (positionname, companyname, positionlength, positiontype, address, companyurl, blurb)
+                   VALUES ('{}','{}','{}','{}','{}','{}', '{}')""".format(
+                   position['positionname'], position['companyname'], position['positionlength'], position['positiontype'],
+                   position['address'], position['companyurl'], position['blurb']))
 
 conn.commit()
 cur.close()
