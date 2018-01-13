a97ea8debcb98c062af3e98a7bcb2ba3443685a0
aidanr002 <arya6609@SHARED.SYDNEY.EDU.AU>
2018-01-13 14:50:50 +1100

    Fixed misspelled 'database'

--- database/setup.py
+++ database/setup.py
@@ -2,8 +2,8 @@ import sqlite3
 conn = sqlite3.connect('database/seekers_personal.db')
 cur = conn.cursor()
 
-cur.executescript(open('databse/seekers_personal.sql', 'rU').read())
-cur.executescript(open('databe/experience.sql', 'rU').read())
+cur.executescript(open('database/seekers_personal.sql', 'rU').read())
+cur.executescript(open('database/experience.sql', 'rU').read())
 conn.commit()
 cur.close()
 conn.close()
