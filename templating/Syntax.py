0660f47e308d40efffa3be2d9fc2756bc229638f
Stanley Su <stanleysu00@gmail.com>
2018-01-13 12:53:01 +1100

    updated templating to allow for for loops

--- templating/Syntax.py
+++ templating/Syntax.py
@@ -5,63 +5,74 @@ from templating.regex import *
 #from nodes import *
 #from regex import *
 
-def generateTree(template, endNode = None):
-    tree = GroupNode()
-    nodeType = "string"
-    index = 0
-    currentNode = None
-    while template != "":
-        # determine nodeType of currentNode
-        if template[0] == "{":
-            nodeType = "template"
-        else:
-            nodeType = "string"
-        # extract current  node
-        if nodeType == "string":
-            nodeContent = ""
-            # if current node is a text node
-            while len(template) and template[0] != "{" :
-                nodeContent += template[0]
-                if len(template) > 1:
-                    template = template[1:]
-                else:
-                    template = ""
-            tree.addChild(TextNode(nodeContent))
-        else:
-            nodeContent = ""
-            numBraces = 0
-            while len(template) and template[0] != "}":
-                if template[0] == "{":
-                    numBraces += 1
-                nodeContent += template[0]
-                if len(template) > 1:
-                    template = template[1:]
-                else:
-                    template = ""
-            nodeContent += template[:numBraces]
-            template = template[numBraces:]
-            for regex in regexes:
-                match = re.search(regexes[regex], nodeContent)
-                if match:
-                    break
+class TemplateTree:
+    def __init__(self,template):
+        self.template = template
+
+    def generateTree(self,endNode = None):
+        tree = GroupNode()
+        nodeType = "string"
+        index = 0
+        currentNode = None
+        while self.template != "":
+            # determine nodeType of currentNode
+            if self.template[0] == "{":
+                nodeType = "template"
             else:
-                continue
-            if endNode:
-                if endNode == 'for' and regex == 'endFor':
-                    return tree
-                # TODO if
-            if regex == 'include':
-                filename = match.group(2)
-                with open(filename) as f:
-                    f = f.read()
-                    tree.addChild(generateTree(f))
-            elif regex == 'expr':
-                tree.addChild(PythonNode(match.group(1)))
-            elif regex == 'for':
-                print(match.groups())
-                tree.addChild(ForNode(match.group(1), match.group(2), generateTree(template, 'for')))
-    return tree
+                nodeType = "string"
+            # extract current  node
+            if nodeType == "string":
+                nodeContent = ""
+                # if current node is a text node
+                while len(self.template) and self.template[0] != "{" :
+                    nodeContent += self.template[0]
+                    if len(self.template) > 1:
+                        self.template = self.template[1:]
+                    else:
+                        self.template = ""
+                tree.addChild(TextNode(nodeContent))
+            else:
+                nodeContent = ""
+                numBraces = 0
+                while len(self.template) and self.template[0] != "}":
+                    if self.template[0] == "{":
+                        numBraces += 1
+                    nodeContent += self.template[0]
+                    if len(self.template) > 1:
+                        self.template = self.template[1:]
+                    else:
+                        self.template = ""
+                nodeContent += self.template[:numBraces]
+                self.template = self.template[numBraces:]
+                for regex in regexes:
+                    match = re.search(regexes[regex], nodeContent)
+                    if match:
+                        break
+                else:
+                    continue
+                if endNode:
+                    if endNode == 'for' and regex == 'endfor':
+                        return tree
+                    # TODO if
+                if regex == 'include':
+                    filename = match.group(2)
+                    with open(filename) as f:
+                        f = f.read()
+                        tree.addChild(TemplateTree(f).generateTree())
+                elif regex == 'expr':
+                    tree.addChild(PythonNode(match.group(1)))
+                elif regex == 'for':
+                    print(match.groups())
+                    tree.addChild(ForNode(match.group(1), match.group(2), self.generateTree('for')))
+        return tree
+
 
 def render_profiles(template, context):
-    tree = generateTree(template)
+    tree = TemplateTree(template).generateTree()
+    print(tree)
     return tree.evaluate(context)
+
+if __name__ == "__main__":
+    with open('sample.html') as f:
+        f = f.read()
+        render_profiles(f, {})
--- templating/nodes.py
+++ templating/nodes.py
@@ -1,5 +1,9 @@
 import html
 
+def indent(s):
+    return ('  ' + s.replace('\n', '\n  '))[:-2]
+    # return s
+
 class Node:
     def evaluate(self):
         pass
@@ -14,6 +18,14 @@ class TextNode(Node):
     def __init__(self, value):
         self.value = value
 
+    def __str__(self):
+        # print(repr(self.value))
+        s = self.value.replace('\n', '\\n')
+        if len(s) < 20:
+            return '\'' + s + '\'\n'
+        else:
+            return '\'' + s[:10] + '...' + s[-10:] + '\'\n'
+
     def evaluate(self, context):
         return str(self.value)
 
@@ -21,6 +33,14 @@ class TextNode(Node):
 class GroupNode(Node):
     def __init__(self):
         self.children = []
+
+    def __str__(self):
+        result = 'Groupnode\n'
+        # print(self.children)
+        for node in self.children:
+            result += indent(str(node))
+            # print(str(node))
+        return result
 
     def addChild(self, newNode):
         self.children.append(newNode)
@@ -36,8 +56,11 @@ class PythonNode(Node):
     def __init__(self, value):
         self.value = value
 
+    def __str__(self):
+        return self.value + '\n'
+
     def evaluate(self, context):
-        return html.escape(eval(self.value, {}, context))
+        return html.escape(str(eval(self.value, {}, context)))
 
 
 class IfNode(Node):
@@ -56,6 +79,9 @@ class ForNode(Node):
         self.variableName = variableName
         self.iterable = iterable
         self.child = child
+
+    def __str__(self):
+        return 'for {} in {}'.format(self.variableName, self.iterable) + str(self.child)
 
     def evaluate(self, context):
         iterable = eval(self.iterable, {}, context)
